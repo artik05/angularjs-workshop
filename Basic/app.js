ad97aed599d25e543af0195010464b8d1c728d84
rameshksh <ramesh.sharma12@gmail.com>
2018-04-11 00:04:35 +0530

    servies

--- Basic/app.js
+++ Basic/app.js
@@ -1,21 +1,27 @@
 // we craete app module
 var app = angular.module('demoApp', []);
 
-// we add controller to app module
-app.controller('MainController', function MainController($scope) {
+// service is used to write business logics
+app.service('DataService', function(){
 
-    // setting title example of oneway binding
-    // $scope act as glue to bind variable with view
-    $scope.title = 'sample title';
-
-    $scope.titles = ['1-Arti', '2-Arti', '3-Arti', '4-Arti'];
-
-    $scope.btnClicked = function () {
-
-        alert('sdfsdfs');
+    this.getEmployee = function(){
+        return [
+            {
+                id: 1,
+                name : 'Arti'
+            },
+            {
+                id: 2,
+                name : 'Ramesh'
+            }
+        ];
     }
 
+    this.getTitles = function(){
+        return ['1-Arti', '2-Arti', '3-Arti', '4-Arti'];
+    }
 });
+
 
 app.directive('myTag', function () {
     return {
@@ -23,3 +29,22 @@ app.directive('myTag', function () {
         templateUrl: 'sample.html'
     };
 });
+
+// we add controller to app module
+app.controller('MainController', function MainController($scope, DataService) {
+
+    // setting title example of oneway binding
+    // $scope act as glue to bind variable with view
+    $scope.title = 'sample title';
+
+    $scope.titles = DataService.getTitles();
+
+    $scope.btnClicked = function () {
+
+        alert('sdfsdfs');
+    }
+
+    console.log(DataService.getEmployee());
+
+});
+
